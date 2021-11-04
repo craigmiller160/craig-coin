3cf5619426a6f0d5602094545edce2371a4a5e35
Craig Miller <craigmiller160@gmail.com>
2021-11-04 17:30:04 -0400

    Fixed lint errors

--- src/routes/mine.ts
+++ src/routes/mine.ts
@@ -3,7 +3,7 @@ import { Express, Request } from 'express';
 import { MineRequest } from '../types/blockTypes';
 
 export const configureMine = (app: Express, blockchain: Blockchain) =>
-	app.post('/mine', (req: Request<{}, {}, MineRequest>, res) => {
+	app.post('/mine', (req: Request<unknown, unknown, MineRequest>, res) => {
 		const block = blockchain.addBlock(req.body.data);
 		console.info(`New block added: ${block}`);
 		res.redirect('/blocks');
