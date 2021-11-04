6c0501270e84eba25984ef5858da36a1376b393c
Craig Miller <craigmiller160@gmail.com>
2021-11-04 16:30:54 -0400

    Added new endpoint to test server

--- test/testutils/testServer.ts
+++ test/testutils/testServer.ts
@@ -1,10 +1,13 @@
 import express, { Express } from 'express';
 import { configureGetBlocks } from '../../src/routes/getBlocks';
 import { Blockchain } from '../../src/chain/Blockchain';
+import {configureMine} from '../../src/routes/mine';
 
 export const createTestServer = (): [Express, Blockchain] => {
 	const blockchain = new Blockchain();
 	const app = express();
 	configureGetBlocks(app, blockchain);
+	configureMine(app, blockchain);
+
 	return [app, blockchain];
 };
