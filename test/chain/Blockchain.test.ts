3684ad22fa3ff56af96f6c3989d28febdfe21d52
Craig Miller <craigmiller160@gmail.com>
2021-11-04 15:06:00 -0400

    Implemented addBlock test for Blockchain.

--- test/chain/Blockchain.test.ts
+++ test/chain/Blockchain.test.ts
@@ -1,11 +1,18 @@
 import { Blockchain } from '../../src/chain/Blockchain';
 
 describe('Blockchain', () => {
-    it('gets read-only chain', () => {
-       throw new Error();
-    });
+	it('starts with genesis block', () => {
+		throw new Error();
+	});
 
-    it('addBlock', () => {
-        throw new Error();
-    });
+	it('addBlock', () => {
+		const blockchain = new Blockchain();
+		expect(blockchain.chain).toHaveLength(1);
+
+		const block = blockchain.addBlock([]);
+		const lastBlock = blockchain.chain[blockchain.chain.length - 1];
+
+		expect(blockchain.chain).toHaveLength(2);
+		expect(JSON.stringify(block)).toEqual(JSON.stringify(lastBlock));
+	});
 });
