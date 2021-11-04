757495abab2dc7a4b962905b488b7595bb01665d
Craig Miller <craigmiller160@gmail.com>
2021-11-04 15:38:58 -0400

    Finished with new tests.

--- src/chain/blockchainUtils.ts
+++ src/chain/blockchainUtils.ts
@@ -1,24 +1,23 @@
 import { Block } from '../block/Block';
 import { genesisBlock, hashBlock } from '../block/blockUtils';
 
-export const isValidChain = (chain: Block[]): boolean => {
+export const isValidChain = (chain: ReadonlyArray<Block>): boolean => {
 	if (JSON.stringify(genesisBlock()) !== JSON.stringify(chain[0])) {
 		return false;
 	}
 
-	return chain
-		.reduce((valid: boolean, block, index, currentChain) => {
-			if (index === 0) {
-				return true;
-			}
+	return chain.reduce((valid: boolean, block, index, currentChain) => {
+		if (index === 0) {
+			return true;
+		}
 
-			if (!valid) {
-				return false;
-			}
+		if (!valid) {
+			return false;
+		}
 
-			const lastBlock = currentChain[index - 1];
-			return (
-				block.lastHash === lastBlock.hash && block.hash === hashBlock(block)
-			);
-		}, true);
+		const lastBlock = currentChain[index - 1];
+		return (
+			block.lastHash === lastBlock.hash && block.hash === hashBlock(block)
+		);
+	}, true);
 };
--- test/chain/blockchainUtils.test.ts
+++ test/chain/blockchainUtils.test.ts
@@ -1,29 +1,36 @@
-import {Block} from '../../src/block/Block';
-import {genesisBlock, mineBlock} from '../../src/block/blockUtils';
-import {isValidChain} from '../../src/chain/blockchainUtils';
+import { Block } from '../../src/block/Block';
+import { genesisBlock, mineBlock } from '../../src/block/blockUtils';
+import { isValidChain } from '../../src/chain/blockchainUtils';
+
+const createChain = (): Block[] =>
+	[...new Array(3).keys()].reduce(
+		(newArray, index) => {
+			const lastBlock = newArray[index];
+			const newBlock = mineBlock(lastBlock, [`${index}`]);
+			return [...newArray, newBlock];
+		},
+		[genesisBlock()]
+	);
 
 describe('blockchainUtils', () => {
-    describe('isValidChain', () => {
-        it('is valid', () => {
-            const chain: Block[] = [...new Array(3).keys()]
-                .reduce((newArray, index) => {
-                    const lastBlock = newArray[index];
-                    const newBlock = mineBlock(lastBlock, [`${index}`]);
-                    return [
-                        ...newArray,
-                        newBlock
-                    ];
-                }, [genesisBlock()]);
-            console.log(chain);
-            expect(isValidChain(chain)).toEqual(true);
-        });
+	describe('isValidChain', () => {
+		it('is valid', () => {
+			const chain = createChain();
+			expect(isValidChain(chain)).toEqual(true);
+		});
 
-        it('invalid genesis block', () => {
-            throw new Error();
-        });
+		it('invalid genesis block', () => {
+			const newGenesis = new Block('timestamp', 'lastHash', 'hash', []);
+			const chain = createChain();
+			chain[0] = newGenesis;
+			expect(isValidChain(chain)).toEqual(false);
+		});
 
-        it('invalid block in chain', () => {
-            throw new Error();
-        });
-    });
+		it('invalid block in chain', () => {
+			const newBlock = new Block('timestamp', 'lastHash', 'hash', []);
+			const chain = createChain();
+			chain.push(newBlock);
+			expect(isValidChain(chain)).toEqual(false);
+		});
+	});
 });
