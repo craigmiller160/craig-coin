af3c5e344c9c5196886d0a9c91119ea4234e063b
Craig Miller <craigmiller160@gmail.com>
2021-11-04 14:46:23 -0400

    Implemented test

--- test/utils/dateUtils.test.ts
+++ test/utils/dateUtils.test.ts
@@ -1,7 +1,13 @@
 import { createTimestamp } from '../../src/utils/dateUtils';
+import {format, utcToZonedTime} from 'date-fns-tz';
 
 describe('dateUtils', () => {
     it('createTimestamp', () => {
-        throw new Error();
+        const expectedTS = format(utcToZonedTime(new Date(), 'UTC'), 'yyyyMMddHHmmssSSSXX', {
+            timeZone: 'UTC'
+        });
+        const actualTS = createTimestamp();
+        expect(actualTS.length).toEqual(expectedTS.length);
+        expect(actualTS.substring(0,12)).toEqual(expectedTS.substring(0,12))
     });
 });
