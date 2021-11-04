35325bdf53a336b5f44cac4e2808f025398572c7
Craig Miller <craigmiller160@gmail.com>
2021-11-04 16:28:33 -0400

    Added mine endpoint to express server

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { Blockchain } from './chain/Blockchain';
 import { configureGetBlocks } from './routes/getBlocks';
 import bodyParser from 'body-parser';
+import {configureMine} from './routes/mine';
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
@@ -10,6 +11,7 @@ const blockchain = new Blockchain();
 const app = express();
 app.use(bodyParser.json());
 configureGetBlocks(app, blockchain);
+configureMine(app, blockchain);
 
 app.listen(HTTP_PORT, () => {
 	console.info(`Listening on port ${HTTP_PORT}`);
