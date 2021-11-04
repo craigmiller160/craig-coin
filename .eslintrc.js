0e76581fe589fa5295d97abca6114adf3f9c86fe
Craig Miller <craigmiller160@gmail.com>
2021-11-04 12:12:36 -0400

    Got prettier working

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,16 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
     extends: [
-        'plugin:@typescript-eslint/recommended'
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended'
     ],
     parserOptions: {
         ecmaVersion: 2020,
         sourceType: 'module'
     },
-    rules: {}
+    rules: {
+        'prettier/prettier': [
+            'error', {}, { usePrettierrc: true }
+        ]
+    }
 }
