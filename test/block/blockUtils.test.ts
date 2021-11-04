bd915d870c844f4ee142a6a4a562be597c56a809
Craig Miller <craigmiller160@gmail.com>
2021-11-04 14:52:39 -0400

    Implemented remaining tests

--- test/block/blockUtils.test.ts
+++ test/block/blockUtils.test.ts
@@ -1,5 +1,6 @@
 import { genesisBlock, mineBlock, hashBlock } from '../../src/block/blockUtils';
 import {format, utcToZonedTime} from 'date-fns-tz';
+import {Block} from '../../src/block/Block';
 
 const getExpectedTs = () => format(
     utcToZonedTime(new Date(), 'UTC'),
@@ -21,10 +22,18 @@ describe('blockUtils', () => {
 	});
 
 	it('mineBlock', () => {
-		throw new Error();
+		const lastBlock = new Block('timestamp', 'lastHash', 'hash', []);
+		const expectedTs = getExpectedTs();
+		const block = mineBlock(lastBlock, []);
+		expect(block.timestamp).toHaveLength(expectedTs.length);
+		expect(block.timestamp.substring(0,12)).toEqual(expectedTs.substring(0,12));
+		expect(block.lastHash).toEqual(lastBlock.hash);
+		expect(block.data).toEqual([]);
+		expect(block.hash).toHaveLength(64);
 	});
 
 	it('hashBlock', () => {
-		throw new Error();
+		const hash = hashBlock('timestamp', 'lastHash', []);
+		expect(hash).toEqual('af5e8e66d42193775e16e0c701ce630760efc63f7c58843c7f3e6685640d7a59');
 	});
 });
