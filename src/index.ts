2c64679321aebb7c28892611fc1beb13b369af34
Craig Miller <craigmiller160@gmail.com>
2021-11-04 17:52:00 -0400

    Got p2p server able to start

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Blockchain } from './chain/Blockchain';
 import { createAndStartRestServer } from './rest-server';
-import {P2pServer} from './p2p-server';
+import { P2pServer } from './p2p-server';
 
 const blockchain = new Blockchain();
 const p2pServer = new P2pServer(blockchain);
