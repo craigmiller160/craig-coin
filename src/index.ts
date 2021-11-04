53589103e0f8738e7d74e8f2dfe957a6e46337ed
Craig Miller <craigmiller160@gmail.com>
2021-11-04 12:33:29 -0400

    Got eslint working again

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Block } from './Block';
-import {createTimestamp} from './utils/createTimestamp';
+import { createTimestamp } from './utils/createTimestamp';
 
+const block = new Block(createTimestamp(), 'abc', 'def', 'Hello World');
 
-const block = new Block(createTimestamp(), 'abc', 'def', 'Hello World')
-
-console.log(block);
+console.log('block');
