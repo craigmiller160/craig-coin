3096e2f9d0f758a46a8011a8824ccafde5e9e897
Craig Miller <craigmiller160@gmail.com>
2021-11-04 15:31:20 -0400

    Implemented the first blockchain test.

--- test/chain/blockchainUtils.test.ts
+++ test/chain/blockchainUtils.test.ts
@@ -1,8 +1,20 @@
+import {Block} from '../../src/block/Block';
+import {genesisBlock, mineBlock} from '../../src/block/blockUtils';
+import {isValidChain} from '../../src/chain/blockchainUtils';
 
 describe('blockchainUtils', () => {
     describe('isValidChain', () => {
         it('is valid', () => {
-            throw new Error();
+            const chain: Block[] = [...new Array(3).keys()]
+                .reduce((newArray, index) => {
+                    const lastBlock = newArray[index];
+                    const newBlock = mineBlock(lastBlock, [`${index}`]);
+                    return [
+                        ...newArray,
+                        newBlock
+                    ];
+                }, [genesisBlock()]);
+            expect(isValidChain(chain)).toEqual(true);
         });
 
         it('invalid genesis block', () => {
