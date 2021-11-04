8dcf542462f2ed8ed1119052c00a93e28b12b0cc
Craig Miller <craigmiller160@gmail.com>
2021-11-04 14:50:11 -0400

    Implemented genesis block test

--- test/block/blockUtils.test.ts
+++ test/block/blockUtils.test.ts
@@ -1,15 +1,30 @@
 import { genesisBlock, mineBlock, hashBlock } from '../../src/block/blockUtils';
+import {format, utcToZonedTime} from 'date-fns-tz';
+
+const getExpectedTs = () => format(
+    utcToZonedTime(new Date(), 'UTC'),
+    'yyyyMMddHHmmssSSSXX',
+    {
+        timeZone: 'UTC'
+    }
+);
 
 describe('blockUtils', () => {
-    it('genesisBlock', () => {
-        throw new Error();
-    });
+	it('genesisBlock', () => {
+	    const expectedTs = getExpectedTs();
+		const block = genesisBlock();
+		expect(block.timestamp).toHaveLength(expectedTs.length);
+		expect(block.timestamp.substring(0,12)).toEqual(expectedTs.substring(0,12));
+		expect(block.lastHash).toEqual('----');
+		expect(block.data).toEqual([]);
+		expect(block.hash).toHaveLength(64);
+	});
 
-    it('mineBlock', () => {
-        throw new Error();
-    });
+	it('mineBlock', () => {
+		throw new Error();
+	});
 
-    it('hashBlock', () => {
-        throw new Error();
-    });
+	it('hashBlock', () => {
+		throw new Error();
+	});
 });
--- test/utils/dateUtils.test.ts
+++ test/utils/dateUtils.test.ts
@@ -1,13 +1,17 @@
 import { createTimestamp } from '../../src/utils/dateUtils';
-import {format, utcToZonedTime} from 'date-fns-tz';
+import { format, utcToZonedTime } from 'date-fns-tz';
 
 describe('dateUtils', () => {
-    it('createTimestamp', () => {
-        const expectedTS = format(utcToZonedTime(new Date(), 'UTC'), 'yyyyMMddHHmmssSSSXX', {
-            timeZone: 'UTC'
-        });
-        const actualTS = createTimestamp();
-        expect(actualTS.length).toEqual(expectedTS.length);
-        expect(actualTS.substring(0,12)).toEqual(expectedTS.substring(0,12))
-    });
+	it('createTimestamp', () => {
+		const expectedTS = format(
+			utcToZonedTime(new Date(), 'UTC'),
+			'yyyyMMddHHmmssSSSXX',
+			{
+				timeZone: 'UTC'
+			}
+		);
+		const actualTS = createTimestamp();
+		expect(actualTS.length).toEqual(expectedTS.length);
+		expect(actualTS.substring(0, 12)).toEqual(expectedTS.substring(0, 12));
+	});
 });
