c0383a7a2a1f594f7670855c7a8d3637802bd2d7
Craig Miller <craigmiller160@gmail.com>
2021-11-04 12:24:34 -0400

    Built more logic in block

--- src/Block.ts
+++ src/Block.ts
@@ -1,8 +1,16 @@
 export class Block {
 	constructor(
-		public timestamp: string,
+		public timestamp: string, // TODO need to create this internally, but also prevent it from changing during mining if i make this immutable
 		public lastHash: string,
 		public hash: string,
 		public data: any // TODO want a type for this
 	) {}
+
+	toString(): string {
+		return `Block - 
+			Timestamp: ${this.timestamp}
+			Last Hash: ${this.lastHash}
+			Hash     : ${this.hash}
+			Data     : ${JSON.stringify(this.data)}`;
+	}
 }
