026809ce6621879ba393a2aec07b298532907df0
Craig Miller <craigmiller160@gmail.com>
2021-11-04 16:32:54 -0400

    Implemented another test for the post endpoint this time.

--- test/routes/mine.test.ts
+++ test/routes/mine.test.ts
@@ -1,7 +1,18 @@
-import { configureMine } from '../../src/routes/mine';
+import {createTestServer} from '../testutils/testServer';
+import request from 'supertest';
 
 describe('mine', () => {
-	it('creates a new block on the blockchain', () => {
-		throw new Error();
+	it('creates a new block on the blockchain', async () => {
+		const body = {
+			data: []
+		};
+		const [app, blockchain] = createTestServer();
+		const response = await request(app)
+			.post('/mine')
+			.send(body)
+			.expect('Content-Type', /application\/json/)
+			.expect(200);
+		expect(blockchain.chain).toHaveLength(2);
+		expect(response.body).toEqual(blockchain.chain);
 	});
 });
