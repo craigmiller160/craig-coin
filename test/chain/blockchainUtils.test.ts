f788523a6e121f3309b9e1bd312d764fab529933
Craig Miller <craigmiller160@gmail.com>
2021-11-04 15:27:34 -0400

    Moved isValidChain tests to new test file.

---
+++ test/chain/blockchainUtils.test.ts
@@ -0,0 +1,16 @@
+
+describe('blockchainUtils', () => {
+    describe('isValidChain', () => {
+        it('is valid', () => {
+            throw new Error();
+        });
+
+        it('invalid genesis block', () => {
+            throw new Error();
+        });
+
+        it('invalid block in chain', () => {
+            throw new Error();
+        });
+    });
+});
