2dbe67a4f5c05f7978a00a4d6254d371a661bffc
Craig Miller <craigmiller160@gmail.com>
2021-11-04 14:01:03 -0400

    Stubbed out more tests

---
+++ test/block/blockUtils.test.ts
@@ -0,0 +1,14 @@
+
+describe('blockUtils', () => {
+    it('genesisBlock', () => {
+        throw new Error();
+    });
+
+    it('mineBlock', () => {
+        throw new Error();
+    });
+
+    it('hashBlock', () => {
+        throw new Error();
+    });
+});
