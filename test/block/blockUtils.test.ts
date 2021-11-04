327ed8cf8ca61df0e05ea2002c3c8edc115658f0
Craig Miller <craigmiller160@gmail.com>
2021-11-04 14:52:56 -0400

    Fixed lint errors

--- test/block/blockUtils.test.ts
+++ test/block/blockUtils.test.ts
@@ -1,21 +1,20 @@
 import { genesisBlock, mineBlock, hashBlock } from '../../src/block/blockUtils';
-import {format, utcToZonedTime} from 'date-fns-tz';
-import {Block} from '../../src/block/Block';
+import { format, utcToZonedTime } from 'date-fns-tz';
+import { Block } from '../../src/block/Block';
 
-const getExpectedTs = () => format(
-    utcToZonedTime(new Date(), 'UTC'),
-    'yyyyMMddHHmmssSSSXX',
-    {
-        timeZone: 'UTC'
-    }
-);
+const getExpectedTs = () =>
+	format(utcToZonedTime(new Date(), 'UTC'), 'yyyyMMddHHmmssSSSXX', {
+		timeZone: 'UTC'
+	});
 
 describe('blockUtils', () => {
 	it('genesisBlock', () => {
-	    const expectedTs = getExpectedTs();
+		const expectedTs = getExpectedTs();
 		const block = genesisBlock();
 		expect(block.timestamp).toHaveLength(expectedTs.length);
-		expect(block.timestamp.substring(0,12)).toEqual(expectedTs.substring(0,12));
+		expect(block.timestamp.substring(0, 12)).toEqual(
+			expectedTs.substring(0, 12)
+		);
 		expect(block.lastHash).toEqual('----');
 		expect(block.data).toEqual([]);
 		expect(block.hash).toHaveLength(64);
@@ -26,7 +25,9 @@ describe('blockUtils', () => {
 		const expectedTs = getExpectedTs();
 		const block = mineBlock(lastBlock, []);
 		expect(block.timestamp).toHaveLength(expectedTs.length);
-		expect(block.timestamp.substring(0,12)).toEqual(expectedTs.substring(0,12));
+		expect(block.timestamp.substring(0, 12)).toEqual(
+			expectedTs.substring(0, 12)
+		);
 		expect(block.lastHash).toEqual(lastBlock.hash);
 		expect(block.data).toEqual([]);
 		expect(block.hash).toHaveLength(64);
@@ -34,6 +35,8 @@ describe('blockUtils', () => {
 
 	it('hashBlock', () => {
 		const hash = hashBlock('timestamp', 'lastHash', []);
-		expect(hash).toEqual('af5e8e66d42193775e16e0c701ce630760efc63f7c58843c7f3e6685640d7a59');
+		expect(hash).toEqual(
+			'af5e8e66d42193775e16e0c701ce630760efc63f7c58843c7f3e6685640d7a59'
+		);
 	});
 });
