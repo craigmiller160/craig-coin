ad603fbaa08f14061ce8d81f2f7a112ecc4ad98c
Craig Miller <craigmiller160@gmail.com>
2021-11-04 12:45:54 -0400

    Got genesis block functionality working.

--- src/Block.ts
+++ src/Block.ts
@@ -1,9 +1,20 @@
+import {createTimestamp} from './utils/createTimestamp';
+
 export class Block {
+	static genesis() {
+		return new this(
+			createTimestamp(),
+			'----',
+			'f1r57-h45h', // TODO generate a first hash anyway
+			[]
+		);
+	}
+
 	constructor(
 		public timestamp: string, // TODO need to create this internally, but also prevent it from changing during mining if i make this immutable
 		public lastHash: string,
 		public hash: string,
-		public data: any // TODO want a type for this
+		public data: any[] // TODO want a type for this
 	) {}
 
 	toString(): string {
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Block } from './Block';
 import { createTimestamp } from './utils/createTimestamp';
 
-const block = new Block(createTimestamp(), 'abc', 'def', 'Hello World');
+const genesis = Block.genesis();
+const block = new Block(createTimestamp(), 'abc', 'def', ['Hello World']);
 
+console.log(genesis);
 console.log(block);
