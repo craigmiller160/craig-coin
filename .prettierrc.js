0f4eda37d05565747ec543c33590d3128d4f08f9
Craig Miller <craigmiller160@gmail.com>
2021-11-04 12:09:36 -0400

    Added prettier

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    semi: true,
+    trailingComma: 'none',
+    singleQuote: true,
+    tabWidth: 4,
+    useTabs: true,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: false,
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: 'always',
+    endOfLine: 'lf',
+    vueIndentScriptAndStyle: true
+};
