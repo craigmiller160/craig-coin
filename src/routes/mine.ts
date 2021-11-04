8a8fd55cd07a2c8b9ab9676da324577272a35151
Craig Miller <craigmiller160@gmail.com>
2021-11-04 16:26:50 -0400

    Fixed lint errors

--- src/routes/mine.ts
+++ src/routes/mine.ts
@@ -1,11 +1,11 @@
 import { Blockchain } from '../chain/Blockchain';
-import {Express} from 'express';
-import {BlockData} from '../types/blockTypes';
+import { Express } from 'express';
+import { BlockData } from '../types/blockTypes';
 
 export const configureMine = (app: Express, blockchain: Blockchain) =>
-    app.post('/mine', (req, res) => {
-        // TODO figure out how to parameterize this better
-        const block = blockchain.addBlock(req.body.data as BlockData);
-        console.info(`New block added: ${block}`);
-        res.redirect('/blocks');
-    });
+	app.post('/mine', (req, res) => {
+		// TODO figure out how to parameterize this better
+		const block = blockchain.addBlock(req.body.data as BlockData);
+		console.info(`New block added: ${block}`);
+		res.redirect('/blocks');
+	});
--- test/routes/mine.test.ts
+++ test/routes/mine.test.ts
@@ -1,7 +1,7 @@
 import { configureMine } from '../../src/routes/mine';
 
 describe('mine', () => {
-    it('creates a new block on the blockchain', () => {
-        throw new Error();
-    });
+	it('creates a new block on the blockchain', () => {
+		throw new Error();
+	});
 });
