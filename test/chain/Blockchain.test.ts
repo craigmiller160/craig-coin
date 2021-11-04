35c0672b2119f077fd10e68bbc517ed8d73d47e5
Craig Miller <craigmiller160@gmail.com>
2021-11-04 15:02:17 -0400

    Stubbed out more tests

---
+++ test/chain/Blockchain.test.ts
@@ -0,0 +1,11 @@
+import { Blockchain } from '../../src/chain/Blockchain';
+
+describe('Blockchain', () => {
+    it('gets read-only chain', () => {
+       throw new Error();
+    });
+
+    it('addBlock', () => {
+        throw new Error();
+    });
+});
