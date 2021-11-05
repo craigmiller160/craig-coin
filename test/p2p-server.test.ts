6aced43e13144747d52dc232f23bd119c1fff6f9
Craig Miller <craigmiller160@gmail.com>
2021-11-05 12:00:40 -0400

    Stubbed out another test

--- test/p2p-server.test.ts
+++ test/p2p-server.test.ts
@@ -4,4 +4,8 @@ describe('p2p-server', () => {
 	it('opens server and connects to peers', () => {
 		throw new Error();
 	});
+
+	it('syncs chain with sockets', () => {
+		throw new Error();
+	});
 });
