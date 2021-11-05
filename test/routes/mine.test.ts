53664932a12df48c5e91391f4122fd3f6c631b4c
Craig Miller <craigmiller160@gmail.com>
2021-11-05 11:59:21 -0400

    Fixed broken test

--- test/routes/mine.test.ts
+++ test/routes/mine.test.ts
@@ -1,6 +1,9 @@
 import request from 'supertest';
 import { Blockchain } from '../../src/chain/Blockchain';
 import { createServer } from '../../src/rest-server';
+import {P2pServer} from '../../src/p2p-server';
+
+jest.mock('../../src/p2p-server');
 
 describe('mine', () => {
 	it('creates a new block on the blockchain', async () => {
@@ -8,11 +11,13 @@ describe('mine', () => {
 			data: []
 		};
 		const blockchain = new Blockchain();
-		const app = createServer(blockchain);
+		const p2pServer = new P2pServer(blockchain);
+		const app = createServer(blockchain, p2pServer);
 		await request(app).post('/mine').send(body).expect(302);
 		expect(blockchain.chain).toHaveLength(2);
 		expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(
 			body.data
 		);
+		expect(p2pServer.syncChains).toHaveBeenCalled();
 	});
 });
