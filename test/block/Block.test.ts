c64125ebaf0c262432c0ca234380e4411a688c6d
Craig Miller <craigmiller160@gmail.com>
2021-11-05 12:31:56 -0400

    Updated test

--- test/block/Block.test.ts
+++ test/block/Block.test.ts
@@ -2,11 +2,12 @@ import { Block } from '../../src/block/Block';
 
 describe('Block', () => {
 	it('prints custom toString()', () => {
-		const block = new Block('timestamp', 'lastHash', 'hash', []);
+		const block = new Block('timestamp', 'lastHash', 'hash', 1,[]);
 		expect(block.toString()).toEqual(`Block - 
 			Timestamp: timestamp
 			Last Hash: lastHash
 			Hash     : hash
+			Nonce    : 1
 			Data     : []`);
 	});
 });
