0313d2daf9a5c21f87bbc8303ee2164985d57248
Craig Miller <craigmiller160@gmail.com>
2021-11-05 13:57:52 -0400

    Got new date utility function working.

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -4,14 +4,12 @@ import { parse } from 'date-fns';
 const FORMAT = 'yyyyMMddHHmmssSSSXX';
 const TIME_ZONE = 'UTC';
 
-export const createTimestamp = () =>
-	format(utcToZonedTime(new Date(), TIME_ZONE), FORMAT, {
+export const createTimestamp = () => millisToTimestamp(Date.now());
+
+export const millisToTimestamp = (millis: number): string =>
+	format(utcToZonedTime(new Date(millis), TIME_ZONE), FORMAT, {
 		timeZone: TIME_ZONE
 	});
-
-export const millisToTimestamp = (millis: number): string => {
-
-};
 
 export const timestampToMillis = (timestamp: string): number => {
 	const date = parse(timestamp, FORMAT, new Date());
