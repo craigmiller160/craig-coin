72fab47bca3a99caca2b41a5313489ea9d6f8b84
Craig Miller <craigmiller160@gmail.com>
2021-11-05 13:43:30 -0400

    Fixed another test.

--- test/block/Block.test.ts
+++ test/block/Block.test.ts
@@ -4,10 +4,11 @@ describe('Block', () => {
 	it('prints custom toString()', () => {
 		const block = new Block('timestamp', 'lastHash', 'hash', 1, []);
 		expect(block.toString()).toEqual(`Block - 
-			Timestamp: timestamp
-			Last Hash: lastHash
-			Hash     : hash
-			Nonce    : 1
-			Data     : []`);
+			Timestamp : timestamp
+			Last Hash : lastHash
+			Hash      : hash
+			Nonce     : 1
+			Difficulty: 2
+			Data      : []`);
 	});
 });
