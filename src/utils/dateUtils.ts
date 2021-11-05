469959afb11a8b2f731130fb623430cd8a9da389
Craig Miller <craigmiller160@gmail.com>
2021-11-05 13:24:34 -0400

    Added millisFromTimestamp function

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,6 +1,15 @@
 import { utcToZonedTime, format } from 'date-fns-tz';
+import { parse } from 'date-fns';
+
+const FORMAT = 'yyyyMMddHHmmssSSSXX';
+const TIME_ZONE = 'UTC';
 
 export const createTimestamp = () =>
-	format(utcToZonedTime(new Date(), 'UTC'), 'yyyyMMddHHmmssSSSXX', {
-		timeZone: 'UTC'
+	format(utcToZonedTime(new Date(), TIME_ZONE), FORMAT, {
+		timeZone: TIME_ZONE
 	});
+
+export const millisFromTimestamp = (timestamp: string): number => {
+	const date = parse(timestamp, FORMAT, new Date());
+	return date.getTime();
+};
