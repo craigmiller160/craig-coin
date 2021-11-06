9d579c0d67beb6af0987f19133b0d7761833c94f
Craig Miller <craigmiller160@gmail.com>
2021-11-06 14:14:30 -0400

    Added id() function

--- src/wallet/walletUtils.ts
+++ src/wallet/walletUtils.ts
@@ -1,5 +1,9 @@
 import { ec } from 'elliptic';
+import { nanoid } from 'nanoid';
 
 const ecInstance = new ec('secp256k1');
 
 export const genKeyPair = (): ec.KeyPair => ecInstance.genKeyPair();
+
+// TODO need test
+export const id = () => nanoid();
