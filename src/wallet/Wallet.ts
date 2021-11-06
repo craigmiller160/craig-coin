304f87b9adf04b6b4a49d2f243828e2fbe7b2769
Craig <craigmiller160@gmail.com>
2021-11-06 08:49:13 -0400

    Integreated keyPair into Wallet

--- src/wallet/Wallet.ts
+++ src/wallet/Wallet.ts
@@ -1,9 +1,11 @@
 import { INITIAL_BALANCE } from '../config';
+import { genKeyPair } from '../chain/chainUtil';
+import { ec } from 'elliptic';
 
 export class Wallet {
 	balance = INITIAL_BALANCE;
-	keyPair = ''; // TODO need type
-	publicKey = '';
+	keyPair: ec.KeyPair = genKeyPair();
+	publicKey = this.keyPair.getPublic().encode('hex', false); // TODO do I want true or false here?
 
 	toString(): string {
 		return `Wallet -
