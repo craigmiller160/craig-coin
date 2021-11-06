52efd0ba9c0de98dab248503afa288706d1d8d86
Craig Miller <craigmiller160@gmail.com>
2021-11-06 14:24:11 -0400

    Stubbed out tests

---
+++ test/transaction/transactionUtils.test.ts
@@ -0,0 +1,13 @@
+export {};
+
+describe('transactionUtils', () => {
+	describe('newTransaction', () => {
+		it('creates transaction', () => {
+			throw new Error();
+		});
+
+		it('exceeds sender balance and cannot create transaction', () => {
+			throw new Error();
+		});
+	});
+});
