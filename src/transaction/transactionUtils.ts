dd4c8d6d2644dedfcc619f0d460db9243e2cc4b0
Craig Miller <craigmiller160@gmail.com>
2021-11-06 15:36:53 -0400

    Further migrating crypto logic

--- src/transaction/transactionUtils.ts
+++ src/transaction/transactionUtils.ts
@@ -2,10 +2,9 @@ import { Wallet } from '../wallet/Wallet';
 import { Transaction } from './Transaction';
 import * as E from 'fp-ts/Either';
 import { createTimestamp } from '../utils/dateUtils';
-import SHA256 from 'crypto-js/sha256';
 import { TransactionInput } from './TransactionInput';
 import { TransactionOutput } from './TransactionOutput';
-import { verifySignature } from '../utils/cryptoUtils';
+import { hashData, verifySignature } from '../utils/cryptoUtils';
 
 export const newTransaction = (
 	senderWallet: Wallet,
@@ -33,15 +32,16 @@ export const newTransaction = (
 		amount: senderWallet.balance,
 		address: senderWallet.publicKey,
 		// TODO why does the input signature sign the outputs?
-		signature: senderWallet.sign(SHA256(JSON.stringify(outputs)).toString())
+		signature: senderWallet.sign(hashData(outputs))
 	};
 
 	return E.right(new Transaction(input, outputs));
 };
+
 // TODO write tests
 export const verifyTransaction = (transaction: Transaction): boolean =>
 	verifySignature(
 		transaction.input.address,
 		transaction.input.signature,
-		SHA256(JSON.stringify(transaction.outputs)).toString()
+		hashData(transaction.outputs)
 	);
--- src/utils/cryptoUtils.ts
+++ src/utils/cryptoUtils.ts
@@ -1,7 +1,9 @@
 import { ec } from 'elliptic';
+import SHA256 from 'crypto-js/sha256';
 
 const ecInstance = new ec('secp256k1');
 
+// TODO migrate tests
 export const genKeyPair = (): ec.KeyPair => ecInstance.genKeyPair();
 
 // TODO write tests
@@ -14,3 +16,11 @@ export const verifySignature = (
 	const publicKey = ecInstance.keyFromPublic(publicKeyString, 'hex');
 	return publicKey.verify(dataHash, signature);
 };
+
+// TODO write tests
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+export const hashData = (data: object | any[]): string =>
+	SHA256(JSON.stringify(data)).toString();
+
+// TODO write tests
+export const hashText = (text: string): string => SHA256(text).toString();
