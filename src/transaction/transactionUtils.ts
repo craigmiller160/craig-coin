40d1866d1569dd149ed477549eb118f36833e4ae
Craig Miller <craigmiller160@gmail.com>
2021-11-06 16:36:33 -0400

    More work on updateTransaction

--- src/transaction/transactionUtils.ts
+++ src/transaction/transactionUtils.ts
@@ -27,15 +27,63 @@ export const newTransaction = (
 		}
 	];
 
-	const input: TransactionInput = {
-		timestamp: createTimestamp(),
-		amount: senderWallet.balance,
-		address: senderWallet.publicKey,
-		// TODO why does the input signature sign the outputs?
-		signature: senderWallet.sign(hashData(outputs))
-	};
-
+	const input: TransactionInput = createTransactionInput(
+		senderWallet,
+		outputs
+	);
 	return E.right(new Transaction(input, outputs));
+};
+
+const createTransactionInput = (
+	senderWallet: Wallet,
+	outputs: ReadonlyArray<TransactionOutput>
+): TransactionInput => ({
+	timestamp: createTimestamp(),
+	amount: senderWallet.balance,
+	address: senderWallet.publicKey,
+	// TODO why does the input signature sign the outputs?
+	signature: senderWallet.sign(hashData(outputs))
+});
+
+export const updateTransaction = (
+	baseTransaction: Transaction,
+	senderWallet: Wallet,
+	recipientAddress: string,
+	amount: number
+): E.Either<Error, Transaction> => {
+	const senderOutputIndex = baseTransaction.outputs.findIndex(
+		(output) => output.address === senderWallet.publicKey
+	);
+
+	if (senderOutputIndex >= 0) {
+		const senderOutput = baseTransaction.outputs[senderOutputIndex];
+		if (amount > senderOutput.amount) {
+			// The only senderOutput that would exist is one referencing the balance of the sender
+			return E.left(new Error(`Amount ${amount} exceeds balance`));
+		}
+
+		const newSenderAmount = senderOutput.amount - amount;
+		const firstPartArray = baseTransaction.outputs.slice(
+			0,
+			senderOutputIndex
+		);
+		const secondPartArray = baseTransaction.outputs.slice(
+			senderOutputIndex + 1
+		);
+		const newOutputs: ReadonlyArray<TransactionOutput> = [
+			...firstPartArray,
+			...secondPartArray,
+			{
+				address: senderWallet.publicKey,
+				amount: newSenderAmount
+			}
+		];
+		const input: TransactionInput = createTransactionInput(
+			senderWallet,
+			newOutputs
+		);
+		return E.right(new Transaction(input, newOutputs));
+	}
 };
 
 export const verifyTransaction = (transaction: Transaction): boolean =>
