8ec57f81431a0e40140f54ac6c00b50f9dc31161
Craig Miller <craigmiller160@gmail.com>
2021-11-06 14:10:29 -0400

    Code tweaks

--- src/chain/chainUtil.ts
+++ src/chain/chainUtil.ts
@@ -1,8 +1,8 @@
-import { ec as EC } from 'elliptic';
+import { ec } from 'elliptic';
 
 // TODO need a new name for this file
 // TODO need tests for this file
 
-const ec = new EC('secp256k1');
+const ecInstance = new ec('secp256k1');
 
-export const genKeyPair = (): EC.KeyPair => ec.genKeyPair();
+export const genKeyPair = (): ec.KeyPair => ecInstance.genKeyPair();
