5b7d99048f25055a87d2e3f05381cc75081c806a
Craig Miller <craigmiller160@gmail.com>
2021-11-06 14:40:48 -0400

    Wrote test for Transaction toString()

--- test/transaction/Transaction.test.ts
+++ test/transaction/Transaction.test.ts
@@ -1,7 +1,17 @@
-export {};
+import { Transaction } from '../../src/transaction/Transaction';
 
 describe('Transaction', () => {
 	it('toString', () => {
-		throw new Error();
+		const outputs = [
+			{
+				amount: 1,
+				address: 'a'
+			}
+		];
+		const transaction = new Transaction(outputs);
+		expect(transaction.toString()).toEqual(`Transaction - 
+		id     : ${transaction.id}
+		input  : 
+		outputs: ${JSON.stringify(outputs, null, 2)}`);
 	});
 });
