fb06c6b943aaad98f604189fbc1d00c19e2eb648
Craig <craigmiller160@gmail.com>
2021-11-13 14:16:29 -0500

    Fixed more tests

--- test/chain/blockchainUtils.test.ts
+++ test/chain/blockchainUtils.test.ts
@@ -11,28 +11,28 @@ describe('blockchainUtils', () => {
 		});
 
 		it('invalid genesis block', () => {
-			const newGenesis = new Block(
-				[],
-				'timestamp',
-				'lastHash',
-				0,
-				DIFFICULTY,
-				'hash'
-			);
+			const newGenesis: Block = {
+				data: [],
+				timestamp: 'timestamp',
+				lastHash: 'lastHash',
+				nonce: 0,
+				difficulty: DIFFICULTY,
+				hash: 'hash'
+			};
 			const chain = createChain();
 			chain[0] = newGenesis;
 			expect(isValidChain(chain)).toEqual(false);
 		});
 
 		it('invalid block in chain', () => {
-			const newBlock = new Block(
-				[],
-				'timestamp',
-				'lastHash',
-				0,
-				DIFFICULTY,
-				'hash'
-			);
+			const newBlock: Block = {
+				data: [],
+				timestamp: 'timestamp',
+				lastHash: 'lastHash',
+				nonce: 0,
+				difficulty: DIFFICULTY,
+				hash: 'hash'
+			};
 			const chain = createChain();
 			chain.push(newBlock);
 			expect(isValidChain(chain)).toEqual(false);
