f7935729e5a7c39f4cc4993070e9545c523852f9
Craig Miller <craigmiller160@gmail.com>
2021-11-14 10:25:27 -0500

    Fixed another compile error

--- src/chain/blockchainUtils.ts
+++ src/chain/blockchainUtils.ts
@@ -1,5 +1,8 @@
 import { Block } from '../block/Block';
 import { genesisBlock, hashBlock } from '../block/blockUtils';
+import { pipe } from 'fp-ts/function';
+import * as E from 'fp-ts/Either';
+import { logger } from '../logger';
 
 export const isValidChain = (chain: ReadonlyArray<Block>): boolean => {
 	if (JSON.stringify(genesisBlock()) !== JSON.stringify(chain[0])) {
@@ -16,9 +19,21 @@ export const isValidChain = (chain: ReadonlyArray<Block>): boolean => {
 		}
 
 		const lastBlock = currentChain[index - 1];
+		const newHash = pipe(
+			hashBlock(block),
+			E.fold(
+				(error) => {
+					logger.error('Error creating block hash for verification');
+					logger.error(error);
+					return '';
+				},
+				(hash) => hash
+			)
+		);
+
 		return (
 			block.lastHash === lastBlock.hash &&
-			block.hash === hashBlock(block) &&
+			block.hash === newHash &&
 			block.hash.startsWith('0'.repeat(block.difficulty))
 		);
 	}, true);
