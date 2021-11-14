8ff878c4acc91432bc210a3780b3726f017de0d6
Craig Miller <craigmiller160@gmail.com>
2021-11-14 10:30:39 -0500

    Fixed more compile errors

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,31 @@ import { createAndStartRestServer } from './rest-server';
 import { P2pServer } from './p2p-server';
 import { TransactionPool } from './transaction/TransactionPool';
 import { Wallet } from './wallet/Wallet';
+import { pipe } from 'fp-ts/function';
+import { genesisBlock } from './block/blockUtils';
+import * as E from 'fp-ts/Either';
+import { logger } from './logger';
 
-const wallet = new Wallet();
-const transactionPool = new TransactionPool();
-const blockchain = new Blockchain();
-
-const p2pServer = new P2pServer(blockchain, transactionPool);
-p2pServer.listen();
-createAndStartRestServer(blockchain, transactionPool, wallet, p2pServer);
+pipe(
+	genesisBlock(),
+	E.map((genesisBlock) => new Blockchain(genesisBlock)),
+	E.fold(
+		(error) => {
+			logger.error('Error starting Blockchain application');
+			logger.error(error);
+		},
+		(blockchain) => {
+			const wallet = new Wallet();
+			const transactionPool = new TransactionPool();
+			const p2pServer = new P2pServer(blockchain, transactionPool);
+			p2pServer.listen();
+			createAndStartRestServer(
+				blockchain,
+				transactionPool,
+				wallet,
+				p2pServer
+			);
+			logger.info('Started Blockchain application');
+		}
+	)
+);
