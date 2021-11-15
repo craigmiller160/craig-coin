f46e19ef788f6b952a1621eb533710bb5cbe5b28
Craig Miller <craigmiller160@gmail.com>
2021-11-14 20:54:11 -0500

    Fixed broken tests

--- test/routes/mine.test.ts
+++ test/routes/mine.test.ts
@@ -1,17 +1,19 @@
 import request from 'supertest';
 import { createTestServer } from '../testutils/createTestServer';
+import { unpackRight } from '../testutils/utilityFunctions';
+import { newTransaction } from '../../src/transaction/transactionUtils';
 
 describe('mine', () => {
 	it('POST /mine', async () => {
-		const body = {
-			data: []
-		};
-		const { app, blockchain, p2pServer } = createTestServer();
-		await request(app).post('/mine').send(body).expect(302);
-		expect(blockchain.chain).toHaveLength(2);
-		expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(
-			body.data
+		const { app, blockchain, p2pServer, transactionPool, wallet } =
+			createTestServer();
+		transactionPool.addTransaction(
+			unpackRight(newTransaction(wallet, 'abc', 100))
 		);
+		await request(app).post('/mine').expect(302);
+		expect(blockchain.chain).toHaveLength(2);
 		expect(p2pServer.syncChains).toHaveBeenCalled();
+		expect(p2pServer.broadcastClearTransactions).toHaveBeenCalled();
+		expect(transactionPool.transactions).toHaveLength(0);
 	});
 });
