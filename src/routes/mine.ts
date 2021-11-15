536ecc7747968651bdbd3fadc970424f4537b719
Craig Miller <craigmiller160@gmail.com>
2021-11-14 20:51:42 -0500

    Completely re-built mine endpoint to mine existing transactions

--- src/routes/mine.ts
+++ src/routes/mine.ts
@@ -1,21 +1,31 @@
 import { Blockchain } from '../chain/Blockchain';
-import { Express, Request } from 'express';
+import { Express } from 'express';
 import { P2pServer } from '../p2p-server';
 import { logger } from '../logger';
-import { BlockData } from '../block/BlockData';
-
-interface MineRequest {
-	readonly data: BlockData;
-}
+import { mine } from '../miner/minerUtils';
+import { TransactionPool } from '../transaction/TransactionPool';
+import { Wallet } from '../wallet/Wallet';
+import { pipe } from 'fp-ts/function';
+import * as E from 'fp-ts/Either';
 
 export const configureMine = (
 	app: Express,
 	blockchain: Blockchain,
+	transactionPool: TransactionPool,
+	wallet: Wallet,
 	p2pServer: P2pServer
 ) =>
-	app.post('/mine', (req: Request<unknown, unknown, MineRequest>, res) => {
-		const block = blockchain.addBlock(req.body.data);
-		logger.info(`New block added: ${block}`);
-		p2pServer.syncChains();
-		res.redirect('/blocks');
+	app.post('/mine', (req, res) => {
+		pipe(
+			mine(blockchain, transactionPool, wallet, p2pServer),
+			E.fold(
+				(error) => {
+					logger.error('Error mining transactions');
+					logger.error(error);
+					res.status(500);
+					res.send(error.message);
+				},
+				() => res.redirect('/blocks')
+			)
+		);
 	});
