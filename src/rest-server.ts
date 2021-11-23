006159ad01da44f93567c7574206315f6efd1081
Craig Miller <craigmiller160@gmail.com>
2021-11-23 17:20:20 -0500

    Setting up security settings

--- src/rest-server.ts
+++ src/rest-server.ts
@@ -12,11 +12,37 @@ import {
 } from './routes/transactions';
 import { Wallet } from './wallet/Wallet';
 import { configureGetWallet } from './routes/wallet';
-import https from 'https';
+import https, { ServerOptions } from 'https';
+import fs from 'fs';
+import path from 'path';
+import nocache from 'nocache';
 
 const HTTP_PORT = process.env.HTTP_PORT
 	? parseInt(process.env.HTTP_PORT)
 	: 3001;
+
+const ciphers = [
+	'ECDHE-ECDSA-AES256-GCM-SHA384',
+	'ECDHE-RSA-AES256-GCM-SHA384',
+	'ECDHE-ECDSA-CHACHA20-POLY1305',
+	'ECDHE-RSA-CHACHA20-POLY1305',
+	'ECDHE-ECDSA-AES128-GCM-SHA256',
+	'ECDHE-RSA-AES128-GCM-SHA256',
+	'ECDHE-ECDSA-AES256-SHA384',
+	'ECDHE-RSA-AES256-SHA384',
+	'ECDHE-ECDSA-AES128-SHA256',
+	'ECDHE-RSA-AES128-SHA256'
+];
+
+const tlsProps: ServerOptions = {
+	key: fs.readFileSync(
+		path.resolve(__dirname, '..', 'certs', 'craigcoin.key.pem')
+	),
+	cert: fs.readFileSync(
+		path.resolve(__dirname, '..', 'certs', 'craigcoin.cert.pem')
+	),
+	ciphers: ciphers.join(';')
+};
 
 export const createServerApplication = (
 	blockchain: Blockchain,
@@ -38,6 +64,8 @@ export const createServerApplication = (
 		transactionPool
 	);
 	configureGetWallet(app, wallet);
+	app.use(nocache());
+	app.disable('x-powered-by');
 	return app;
 };
 
@@ -53,7 +81,7 @@ export const createAndStartRestServer = (
 		wallet,
 		p2pServer
 	);
-	const server = https.createServer(app);
+	const server = https.createServer(tlsProps, app);
 	server.listen(HTTP_PORT, () => {
 		logger.info(`Listening on port ${HTTP_PORT}`);
 	});
