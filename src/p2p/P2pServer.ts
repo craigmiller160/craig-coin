3ed8fdec79ebda0689a719d7b525e7bc9ce83248
Craig <craigmiller160@gmail.com>
2021-11-24 12:43:28 -0500

    More work on P2pServer

--- src/p2p/P2pServer.ts
+++ src/p2p/P2pServer.ts
@@ -1,11 +1,30 @@
-import WebSocket from 'ws';
+import WebSocket, { Server as WsServer } from 'ws';
 import { Blockchain } from '../chain/Blockchain';
 import { TransactionPool } from '../transaction/TransactionPool';
+import { Server as HttpsServer } from 'https';
+import { createHttpsServer } from '../tls';
+import { logger } from '../logger';
+
+const P2P_PORT = process.env.P2P_PORT ? parseInt(process.env.P2P_PORT) : 5001;
 
 export class P2pServer {
 	#sockets: ReadonlyArray<WebSocket> = [];
+	readonly #webSocketServer: WsServer;
+	readonly #httpsServer: HttpsServer;
 	constructor(
+		// TODO see if these dependencies can be eliminated
 		public readonly blockchain: Blockchain,
 		public readonly transactionPool: TransactionPool
-	) {}
+	) {
+		this.#httpsServer = createHttpsServer();
+		this.#webSocketServer = new WsServer({
+			server: this.#httpsServer
+		});
+		// TODO need to configure the on() callback
+	}
+
+	listen() {
+		logger.info(`Listening for peer-to-peer connections on: ${P2P_PORT}`);
+		this.#httpsServer.listen(P2P_PORT);
+	}
 }
