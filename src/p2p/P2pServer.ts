c3c165ff1fc44fa0a5aaeda905421f63ef3961a3
Craig <craigmiller160@gmail.com>
2021-11-24 12:44:50 -0500

    Improved error handling of listen() function on new P2pServer

--- src/p2p/P2pServer.ts
+++ src/p2p/P2pServer.ts
@@ -4,6 +4,8 @@ import { TransactionPool } from '../transaction/TransactionPool';
 import { Server as HttpsServer } from 'https';
 import { createHttpsServer } from '../tls';
 import { logger } from '../logger';
+import * as E from 'fp-ts/Either';
+import { unknownToError } from '../utils/unknownToError';
 
 const P2P_PORT = process.env.P2P_PORT ? parseInt(process.env.P2P_PORT) : 5001;
 
@@ -23,8 +25,12 @@ export class P2pServer {
 		// TODO need to configure the on() callback
 	}
 
-	listen() {
-		logger.info(`Listening for peer-to-peer connections on: ${P2P_PORT}`);
-		this.#httpsServer.listen(P2P_PORT);
+	listen(): E.Either<Error, void> {
+		return E.tryCatch(() => {
+			logger.info(
+				`Listening for peer-to-peer connections on: ${P2P_PORT}`
+			);
+			this.#httpsServer.listen(P2P_PORT);
+		}, unknownToError);
 	}
 }
