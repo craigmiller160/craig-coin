84f6471bcecf28cb110b5535b30ce08e0e713902
Craig <craigmiller160@gmail.com>
2021-11-24 18:20:59 -0500

    Setting up test wrappers

---
+++ test/p2p/TestWebSocketWrappers.ts
@@ -0,0 +1,38 @@
+import {
+	WebSocketHttpsServerWrapper,
+	WebSocketServerWrapper,
+	WebSocketWrapper
+} from '../../src/p2p/WebSocketWrappers';
+
+export class TestWebSocketWrapper implements WebSocketWrapper {
+	events: { [name: string]: [(message?: string) => void] } = {};
+	sentData: string[] = [];
+
+	on(event: string, fn: (message?: string) => void): void {
+		const existing = this.events[event] ?? [];
+		existing.push(fn);
+		this.events[event] = existing;
+	}
+
+	send(data: string): void {
+		this.sentData.push(data);
+	}
+}
+
+export class TestWebSocketHttpsServerWrapper
+	implements WebSocketHttpsServerWrapper
+{
+	listenPort = 0;
+	listen(port: number): void {
+		this.listenPort = port;
+	}
+}
+
+export class TestWebSocketServerWrapper implements WebSocketServerWrapper {
+	events: { [name: string]: [(socket: WebSocketWrapper) => void] } = {};
+	on(event: string, fn: (socket: WebSocketWrapper) => void): void {
+		const existing = this.events[event] ?? [];
+		existing.push(fn);
+		this.events[event] = existing;
+	}
+}
