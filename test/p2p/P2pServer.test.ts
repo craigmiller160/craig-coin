41fcaefa75e7a0da341ab4415d2dc2bb00f792d7
Craig <craigmiller160@gmail.com>
2021-11-25 13:41:18 -0500

    Stubbed out tests

---
+++ test/p2p/P2pServer.test.ts
@@ -0,0 +1,21 @@
+export {};
+
+describe('P2pServer', () => {
+	it('addConnectedSocket', () => {
+		throw new Error();
+	});
+
+	describe('updateSockets', () => {
+		it('same length', () => {
+			throw new Error();
+		});
+
+		it('new length', () => {
+			throw new Error();
+		});
+	});
+
+	it('listen', () => {
+		throw new Error();
+	});
+});
