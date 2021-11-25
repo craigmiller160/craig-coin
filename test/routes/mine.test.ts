2bdacdc87170f785cd777f807a33678af9470ecf
Craig <craigmiller160@gmail.com>
2021-11-25 10:36:33 -0500

    Fixed more broken tests

--- test/routes/mine.test.ts
+++ test/routes/mine.test.ts
@@ -2,11 +2,23 @@ import request from 'supertest';
 import { createTestServer } from '../testutils/createTestServer';
 import { unpackRight } from '../testutils/utilityFunctions';
 import { newTransaction } from '../../src/transaction/transactionUtils';
+import {
+	broadcastBlockchain,
+	broadcastClearTransactions
+} from '../../src/p2p/p2pUtils';
+
+jest.mock('../../src/p2p/p2pUtils', () => {
+	const p2pUtils = jest.requireActual('../../src/p2p/p2pUtils');
+	return {
+		...p2pUtils,
+		broadcastClearTransactions: jest.fn(),
+		broadcastBlockchain: jest.fn()
+	};
+});
 
 describe('mine', () => {
 	it('POST /mine', async () => {
-		const { app, blockchain, p2pServer, transactionPool, wallet } =
-			createTestServer();
+		const { app, blockchain, transactionPool, wallet } = createTestServer();
 		transactionPool.addTransaction(
 			unpackRight(newTransaction(wallet, 'abc', 100))
 		);
@@ -18,8 +30,8 @@ describe('mine', () => {
 			)
 			.expect(302);
 		expect(blockchain.chain).toHaveLength(2);
-		expect(p2pServer.syncChains).toHaveBeenCalled();
-		expect(p2pServer.broadcastClearTransactions).toHaveBeenCalled();
+		expect(broadcastBlockchain).toHaveBeenCalled();
+		expect(broadcastClearTransactions).toHaveBeenCalled();
 		expect(transactionPool.transactions).toHaveLength(0);
 	});
 });
