f336aa48241ad59ed275d65f3b4decccce13b1f6
Craig <craigmiller160@gmail.com>
2021-11-26 11:46:49 -0500

    Implemented more tests

--- test/io/keys.test.ts
+++ test/io/keys.test.ts
@@ -1,7 +1,13 @@
-import { keyFileExists, loadPrivateKey } from '../../src/io/keys';
+import {
+	keyFileExists,
+	loadPrivateKey,
+	savePrivateKey
+} from '../../src/io/keys';
 import { unpackRight } from '../testutils/utilityFunctions';
 import { getDataDirPath } from '../../src/io/constants';
 import path from 'path';
+import '@relmify/jest-fp-ts';
+import fs from 'fs';
 
 jest.mock('../../src/io/constants', () => {
 	return {
@@ -11,9 +17,21 @@ jest.mock('../../src/io/constants', () => {
 
 const testfilesPath = path.resolve(__dirname, '..', 'testfiles');
 
+const cleanupDirectory = () => {
+	fs.rmSync(path.resolve(process.cwd(), 'keys'), {
+		recursive: true,
+		force: true
+	});
+};
+
 describe('io keys', () => {
 	beforeEach(() => {
 		(getDataDirPath as jest.Mock).mockReset();
+		cleanupDirectory();
+	});
+
+	afterEach(() => {
+		cleanupDirectory();
 	});
 
 	it('keyFileExists', () => {
@@ -28,6 +46,11 @@ describe('io keys', () => {
 	});
 
 	it('savePrivateKey', () => {
-		throw new Error();
+		(getDataDirPath as jest.Mock).mockImplementation(() => process.cwd());
+		const result = savePrivateKey('Hello World');
+		expect(result).toBeRight();
+		expect(
+			fs.existsSync(path.resolve(process.cwd(), 'keys', 'privateKey.pem'))
+		);
 	});
 });
