6bcfbc57edc97d8c4814d67babb7335ebc603988
Craig <craigmiller160@gmail.com>
2021-11-25 21:13:13 -0500

    Making some progress

--- src/setup/setupWallet.ts
+++ src/setup/setupWallet.ts
@@ -1,12 +1,32 @@
 import os from 'os';
 import path from 'path';
 import fs from 'fs';
+import * as E from 'fp-ts/Either';
 
 const DATA_DIR_ROOT = path.resolve(os.homedir(), '.craigcoin');
 const dataDirPath = !!process.env.DATA_DIR_NAME
 	? path.resolve(DATA_DIR_ROOT, process.env.DATA_DIR_NAME)
 	: DATA_DIR_ROOT;
+const keyDirPath = path.resolve(dataDirPath, 'keys');
+const publicKeyPath = path.resolve(keyDirPath, 'public.pem');
+const privateKeyPath = path.resolve(keyDirPath, 'private.pem');
+
+const keyFilesExist = (): boolean =>
+	fs.existsSync(path.resolve(publicKeyPath)) &&
+	fs.existsSync(path.resolve(privateKeyPath));
+
+const loadKeys = (): E.Either<Error, [string,string]> => {
+    // TODO finish this
+};
+
+const saveKeys = (publicKey: string, privateKey: string) => {
+    // TODO finish this
+}
 
 export const setupWallet = () => {
-	// TODO finish this
+	if (keyFilesExist()) {
+        loadKeys();
+    } else {
+        // TODO gen keys and save them
+    }
 };
