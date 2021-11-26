ec01946e6a042f4927433af535d96f9b5ab72e94
Craig <craigmiller160@gmail.com>
2021-11-26 10:51:25 -0500

    Figured out how to preserve elliptic keys

---
+++ src/temp.ts
@@ -0,0 +1,18 @@
+import { ec } from 'elliptic';
+
+// TODO delete this file
+
+const ecInstance = new ec('secp256k1');
+const keyPair = ecInstance.genKeyPair();
+const pubString = keyPair.getPublic(false, 'hex');
+const privString = keyPair.getPrivate('hex');
+console.log('Public', pubString);
+console.log('Private', privString);
+
+const keyPair2 = ecInstance.keyFromPrivate(privString, 'hex');
+console.log('Public2', keyPair2.getPublic('hex'));
+console.log('Private2', keyPair2.getPrivate('hex'));
+
+const keyPair3 = ecInstance.keyFromPublic(pubString, 'hex');
+console.log('Public3', keyPair3.getPublic('hex'));
+console.log('Private3', keyPair3.getPrivate('hex'));
