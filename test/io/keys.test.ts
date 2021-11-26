3ba8250586260dd1945ca64ac51956173666abe8
Craig <craigmiller160@gmail.com>
2021-11-26 11:16:42 -0500

    Stubbed out more tests

---
+++ test/io/keys.test.ts
@@ -0,0 +1,21 @@
+export {};
+
+describe('io keys', () => {
+	describe('keyFileExists', () => {
+		it('does exist', () => {
+			throw new Error();
+		});
+
+		it('does not exist', () => {
+			throw new Error();
+		});
+	});
+
+	it('loadPrivateKey', () => {
+		throw new Error();
+	});
+
+	it('savePrivateKey', () => {
+		throw new Error();
+	});
+});
