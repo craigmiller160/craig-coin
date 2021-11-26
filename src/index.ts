7ab1a57a4ebbf882783d22a7ceedc30a0d593acd
Craig <craigmiller160@gmail.com>
2021-11-25 21:05:33 -0500

    Moved a bunch of setup logic into separate files

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,10 @@
-import { Blockchain } from './chain/Blockchain';
 import { createAndStartRestServer } from './rest-server';
-import { TransactionPool } from './transaction/TransactionPool';
 import { Wallet } from './wallet/Wallet';
 import { pipe } from 'fp-ts/function';
-import { genesisBlock } from './block/blockUtils';
 import * as E from 'fp-ts/Either';
 import { logger } from './logger';
-import { connectToPeers, createP2pServer } from './p2p/p2pUtils';
-import { P2pServer } from './p2p/P2pServer';
-
-type ChainAndPool = [Blockchain, TransactionPool];
-type ChainPoolAndServer = [Blockchain, TransactionPool, P2pServer];
-
-const setupBlockchainAndTransactionPool = (): E.Either<Error, ChainAndPool> =>
-	pipe(
-		genesisBlock(),
-		E.map((genesisBlock): ChainAndPool => {
-			const blockchain = new Blockchain(genesisBlock);
-			const transactionPool = new TransactionPool();
-			return [blockchain, transactionPool];
-		})
-	);
-
-const setupP2pServer = (
-	blockchain: Blockchain,
-	transactionPool: TransactionPool
-): E.Either<Error, ChainPoolAndServer> =>
-	pipe(
-		createP2pServer(blockchain, transactionPool),
-		E.chain((p2pServer) =>
-			pipe(
-				p2pServer.listen(),
-				E.map(() => p2pServer)
-			)
-		),
-		E.map((p2pServer) => {
-			connectToPeers(p2pServer, blockchain, transactionPool);
-			return p2pServer;
-		}),
-		E.map(
-			(p2pServer): ChainPoolAndServer => [
-				blockchain,
-				transactionPool,
-				p2pServer
-			]
-		)
-	);
+import { setupBlockchainAndTransactionPool } from './setup/setupBlockchainAndTransactionPool';
+import { setupP2pServer } from './setup/setupP2pServer';
 
 pipe(
 	setupBlockchainAndTransactionPool(),
